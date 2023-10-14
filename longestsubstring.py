b0d7a66568581cb90be42b1ac181225de2fbecbf
NidhuN <100463732+Nidhun-vp@users.noreply.github.com>
2023-10-14 08:09:18 -0400

    Add files via upload

---
+++ longestsubstring.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if s=="":
+            return 0
+        start=0
+        end=0
+        maxLength=0
+        unique_characters=set()
+        while end<len(s):
+            if s[end] not in unique_characters:
+                unique_characters.add(s[end])
+                end=end+1
+                maxLength=max(maxLength,len(unique_characters))
+            else:
+                unique_characters.remove(s[start])
+                start=start+1
+        return maxLength        
+            
